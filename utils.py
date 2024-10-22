15b39534f1296e7ce6f02e1bb1615d777a915589
Kamil Jędryczek <kamillo.j@gmail.com>
2024-10-22 14:36:47 +0200

    Sending reamaining notebooks

--- utils.py
+++ utils.py
@@ -1,42 +1,8 @@
-import os
-import io
-import pyAesCrypt
 import pandas as pd
-import pickle
 import traitlets
 from ipywidgets import widgets
 from IPython.display import display
 from tkinter import Tk, filedialog
-
-
-def decrypt_file(file_path, password):
-    if password is None or password == '':
-        raise ValueError(f'{bcolors.BOLD}{bcolors.FAIL}Please provide password')
-        
-    bufferSize = 64 * 1024
-    encFileSize = os.stat(file_path).st_size
-
-    fIn = open(file_path, 'rb')
-    fDec = io.BytesIO()
-
-    pyAesCrypt.decryptStream(fIn, fDec, password, bufferSize, encFileSize)
-    
-    return fDec
-
-
-def decrypt_pandas(file_path, password, sep=','):        
-    fDec = decrypt_file(file_path, password)
-
-    s=str(fDec.getvalue(),'utf-8')
-
-    data = io.StringIO(s) 
-    return pd.read_table(data, sep=sep)
-
-
-def decrypt_pickle(file_path, password):
-    fDec = decrypt_file(file_path, password)
-
-    return pickle.loads(fDec.getvalue())
 
 
 class SelectFilesButton(widgets.Button):
